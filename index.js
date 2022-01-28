372564b1881fbf1abdee3c6247cd2e26412488b7
Emily LD <lagreouemily@gmail.com>
2022-01-28 21:36:55 +0100

    back files

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+require("dotenv").config();
+
+//Package mailgun-js
+const mailgun = require("mailgun-js")({
+  apiKey: process.env.API_KEY,
+  domain: process.env.DOMAIN,
+});
+
+const app = express();
+app.use(cors());
+app.use(formidable());
+
+app.post("/form", (req, res) => {
+  console.log(req.fields);
+  const data = {
+    from: `${req.fields.firstname} ${req.fields.lastname} <${req.fields.email}>`,
+    to: "lagreouemily@gmail.com",
+    subject: "Hello",
+    text: `${req.fields.message}`,
+  };
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+    if (error === undefined) {
+      res.json({ message: "Mail sent", body: body });
+    } else {
+      res.json(error);
+    }
+  });
+});
+
+app.all("*", (req, res) => {
+  res.json({ message: "Page not found" });
+});
+
+app.listen(4000, () => {
+  console.log("Server has started.");
+});
