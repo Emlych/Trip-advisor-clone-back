81401f75f66fa599ed7c981a69b37c0207615c08
Emily LD <lagreouemily@gmail.com>
2022-01-29 09:28:11 +0100

    add try catch to index.js

--- index.js
+++ index.js
@@ -3,30 +3,35 @@ const formidable = require("express-formidable");
 const cors = require("cors");
 require("dotenv").config();
 
+const app = express();
+app.use(cors());
+app.use(formidable());
+
 //Package mailgun-js
 const mailgun = require("mailgun-js")({
   apiKey: process.env.API_KEY,
   domain: process.env.DOMAIN,
 });
 
-const app = express();
-app.use(cors());
-app.use(formidable());
-
 app.post("/form", (req, res) => {
-  const data = {
-    from: `${req.fields.firstname} ${req.fields.lastname} <${req.fields.email}>`,
-    to: "lagreouemily@gmail.com",
-    subject: "Hello",
-    text: `${req.fields.message}`,
-  };
-  mailgun.messages().send(data, (error, body) => {
-    if (error === undefined) {
-      res.json({ message: "Mail sent", body: body });
-    } else {
-      res.json(error);
-    }
-  });
+  try {
+    const data = {
+      from: `${req.fields.firstname} ${req.fields.lastname} <${req.fields.email}>`,
+      to: process.env.MAIL,
+      subject: "Hello",
+      text: `${req.fields.message}`,
+    };
+    mailgun.messages().send(data, (error, body) => {
+      if (error === undefined) {
+        res.json({ message: "Mail sent", body: body });
+      } else {
+        res.status(400).json(error);
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.all("*", (req, res) => {
